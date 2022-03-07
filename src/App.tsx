46d3d14af8e18ca77848390df160b0e4899f3977
KoSakaki <sakaki@flaginc.net>
2022-03-07 18:53:30 +0900

    forth commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-
-import { Editor } from './pages/editor'
-import { createGlobalStyle } from 'styled-components'
+import { Editor } from "./pages/editor";
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body * {
     box-sizing: border-box;
   }
-`
+`;
 function App() {
-
   return (
     <>
-     <GlobalStyle />
+      <GlobalStyle />
       <Editor />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/hooks/use_state_with_strage.ts
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+
+export const useStateWithStrage = (init:string, key:string): [string, (s:string) => viod] => {
+
+  const [value, setValue] = useState<string>(localStorage.getItem(key) || init);
+  const setValueWithStrage = (nextValue:string):viod => {
+    setValue(nextValue);
+    localStorage.setItem(key, nextValue);
+ }
+  return [value, setValueWithStrage];
+}
